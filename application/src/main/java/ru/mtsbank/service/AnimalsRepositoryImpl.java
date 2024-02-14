0649904dc5424b6713b6470f41bf76b749834cf4
grpklim <grpklim@gmail.com>
2024-02-14 16:15:42 +0300

    дз7

--- application/src/main/java/ru/mtsbank/service/AnimalsRepositoryImpl.java
+++ application/src/main/java/ru/mtsbank/service/AnimalsRepositoryImpl.java
@@ -6,10 +6,7 @@ import ru.mtsbank.animals.Animal;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Repository
 public class AnimalsRepositoryImpl implements AnimalsRepository {
--- application/src/test/java/ru/mtsbank/service/TestRepository.java
+++ application/src/test/java/ru/mtsbank/service/TestRepository.java
@@ -1,9 +1,7 @@
 package ru.mtsbank.service;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.mtsbank.animals.*;
