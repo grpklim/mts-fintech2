47ab5ed69f792f45cd025499468c60747bce3859
grpklim <grpklim@gmail.com>
2024-02-15 18:09:37 +0300

    repository

--- application/src/main/java/ru/mtsbank/app/Application.java
+++ application/src/main/java/ru/mtsbank/app/Application.java
@@ -2,11 +2,9 @@ package ru.mtsbank.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ComponentScan("ru")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru"})
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
